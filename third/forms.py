3d17ef6de34c6530be49c4513054f0f74f8eb646
yjk <kyj8028@hotmail.com>
2020-02-16 21:51:57 +0900

    #3.1 Lecture Completed

--- third/forms.py
+++ third/forms.py
@@ -35,18 +35,37 @@ class ReviewForm(ModelForm):
 class RestaurantForm(ModelForm):
     class Meta:
         model = Restaurant
-        fields = ['name', 'address']
+        fields = ['name', 'address', 'image', 'password']
         labels = {
             'name': _('이름'),
             'address': _('주소'),
+            'image': _('이미지 url'),
+            'password': _('게시물 비밀번호')
         }
         help_texts = {
-            'name': _('이름을 입력해주세요.'),
-            'address': _('주소를 입력해주세요.'),
+            'name': _('이름을 입력해 주세요.'),
+            'address': _('주소를 입력해 주세요.'),
+            'image': _('이미지의 url을 입력해 주세요.'),
+            'password': _('게시물의 비밀번호를 입력해 주세요.')
         }
-
+        widgets = {
+            'password': forms.PasswordInput()
+        }
         error_messages = {
             'name': {
                 'max_length': _('이름이 너무 길어요. 30자 이하로 해주세요.')
-            }
+            },
+            'image': {
+                'max_length': _('이미지 주소가 너무 길어요. 500자 이하로 해주세요.')
+            },
+            'password': {
+                'max_length': _('비밀번호가 너무 길어요. 20자 이하로 해주세요.')
+            },
         }
+
+
+class UpdateRestaurntForm(RestaurantForm):
+    class Meta:
+        model = Restaurant
+        # password는 처음 생성할 떄 외에는 데이터에 계속 쓰여지면 안되고 맞는지 검증만 해야 하기 떄문에 제외시켜야 한다.
+        exclude = ['password']
--- third/views.py
+++ third/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from third.models import Restaurant
+from third.models import Restaurant, Review
 from django.core.paginator import Paginator
-from third.forms import RestaurantForm, ReviewForm
+from third.forms import RestaurantForm, ReviewForm, UpdateRestaurntForm
 from django.http import HttpResponseRedirect
-
+from django.db.models import Count, Avg
 
 # Create your views here.
 def list(request):
-    restaurants = Restaurant.objects.all()
+    # annotate 활용 study ! Count 안에 있는 review는 models의 class 내에 있는 변수다 (보이지 않아도 있음)
+    restaurants = Restaurant.objects.all().annotate(reviews_count=Count('review')).annotate(average_point=Avg('review__point'))
     paginator = Paginator(restaurants, 5)
     
     page = request.GET.get('page')
@@ -31,8 +32,9 @@ def update(request):
     if request.method == 'POST' and 'id' in request.POST:
         # item = Restaurant.objects.get(pk=request.POST.get('id'))
         item = get_object_or_404(Restaurant, pk=int(request.POST.get('id')))
-        form = RestaurantForm(request.POST, instance=item) # instance=item이어야 create가 아니라 update가 된다.
-        if form.is_valid():
+        password = request.POST.get('password', '')
+        form = UpdateRestaurntForm(request.POST, instance=item) # instance=item이어야 create가 아니라 update가 된다.
+        if form.is_valid() and password == item.password:
             item = form.save()
             
     elif request.method == 'GET':
@@ -46,15 +48,19 @@ def update(request):
 def detail(request, id):
     if 'id' is not None:
         item = get_object_or_404(Restaurant, pk=id)
-        return render(request, 'third/detail.html', {'item': item})
+        reviews = Review.objects.filter(restaurant=item).all()
+        return render(request, 'third/detail.html', {'item': item, 'reviews':reviews})
     return HttpResponseRedirect('third/list/')
 
 
-def delete(request):
-    if 'id' in request.GET:
-        item = get_object_or_404(Restaurant, pk=int(request.GET.get('id')))
-        item.delete()
-    return HttpResponseRedirect('/third/list/')
+def delete(request, id):
+    item = get_object_or_404(Restaurant, pk=id)
+    if request.method == 'POST' and 'password' in request.POST:
+        if item.password == request.POST.get('password') or item.password is None:
+            item.delete()
+            return redirect('list')
+        return redirect('restaurant-detail', id=id)
+    return render(request, 'third/delete.html', {'item': item})
 
 
 def review_create(request, restaurant_id):
@@ -68,3 +74,26 @@ def review_create(request, restaurant_id):
     form = ReviewForm(initial={'restaurant': item})
 
     return render(request, 'third/review_create.html', {'form':form, 'item':item})
+
+
+def review_delete(request, restaurant_id, review_id):
+    item = get_object_or_404(Review, pk=review_id)
+    item.delete()
+
+    return redirect('restaurant-detail', id=restaurant_id)
+
+
+def review_list(request):
+    # select_related()를 써서 페이지에서 데이터를 호출하는 횟수를 (리뷰갯수)회에서 (1)회로 줄일 수 있다. 성능 향상.
+    reviews = Review.objects.all().select_related().order_by('-created_at')
+    paginator = Paginator(reviews, 10)
+
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+
+    context = {
+        'reviews':items
+    }
+
+    return render(request, 'third/review_list.html', context)
+
